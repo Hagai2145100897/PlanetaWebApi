7fdcb12d69e5f1966328b0a4c259fc29a0bc722d
unknown <Hagai>
2021-07-30 23:04:22 +0500

    clear trash

--- PlanetaWebApi/Controllers/ClientSubnetsController.cs
+++ PlanetaWebApi/Controllers/ClientSubnetsController.cs
@@ -13,45 +13,10 @@ namespace PlanetaWebApi.Controllers
             Repository = repository;
         }
 
-        // [HttpGet(Name = "GetAllClients")]
-        // public IEnumerable<ClientItem> Get()
-        // {
-        //     return Repository.Get();
-        // }
-
         [HttpGet("{clientId}", Name = "GetClientSubnets")]
         public IActionResult Get(int clientId)
         {
             return new ObjectResult(Repository.Get(clientId));
         }
-
-        // [HttpPost]
-        // public IActionResult Create([FromBody] ClientItem clientItem)
-        // {
-        //     if (clientItem == null)
-        //     {
-        //         return BadRequest();
-        //     }
-        //     Repository.Create(clientItem);
-        //     return CreatedAtRoute("GetClientItem", new { id = clientItem.Id }, clientItem);
-        // }
-
-        // [HttpPut("{id}")]
-        // public IActionResult Update(int id, [FromBody] ClientItem updatedClientItem)
-        // {
-        //     if (updatedClientItem == null || updatedClientItem.Id != id)
-        //     {
-        //         return BadRequest();
-        //     }
-
-        //     var clientItem = Repository.Get(id);
-        //     if (clientItem == null)
-        //     {
-        //         return NotFound();
-        //     }
-
-        //     Repository.Update(updatedClientItem);
-        //     return RedirectToRoute("GetAllClients");
-        // }
     }
 }
